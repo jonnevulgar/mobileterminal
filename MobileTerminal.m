7b8d523846ccb04b11152c69acf8cea13324ac4f
tsangk <tsangk@58fa8ae0-6a36-0410-a7a1-b30e0234c60e>
2007-08-02 03:21:20 +0000

    Control character support, "rolled" back softtab clean up commit from allen.porter: too lazy to merge.  Need to have talk about hard tabs vs 2 space soft tabs.  Face wrath over IRC later.
    Make a bullet character then a letter to make the approprate control key.

--- MobileTerminal.m
+++ MobileTerminal.m
@@ -10,8 +10,8 @@
 #import <UIKit/UIView-Rendering.h>
 #import <UIKit/UITextView.h>
 #import <UIKit/UIKeyboard.h>
-// TODO: UIWebView is missing a header
-//#import <UIKit/UIWebView.h>
+
+//#import "DelegateDebug.h"
 
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -19,6 +19,13 @@
 #include <util.h>
 #include <pthread.h>
 #include <stdlib.h>
+
+//#define DEBUG
+#ifdef DEBUG
+    #define debug(...) NSLog(__VA_ARGS__)
+#else
+    #define debug(...)
+#endif
 
 int fd;
 
@@ -31,12 +38,12 @@ int fd;
  
 - (BOOL)autoCapitalizationPreference
 {
-  return false;
+	return false;
 }
 
 - (BOOL)autoCorrectionPreference
 {
-  return false;
+	return false;
 }
 
 @end
@@ -51,17 +58,19 @@ int fd;
 
 - (void)drawRect:(struct CGRect)fp8
 {
+	
 }
 
 @end
 
 @interface ShellView : UITextView {
-  NSMutableString* _nextCommand;
-  bool _ignoreInsertText;
+	NSMutableString* _nextCommand;
+	bool _ignoreInsertText;
+	bool _controlKeyMode;
 }
 - (id)initWithFrame:(struct CGRect)fp8;
 - (BOOL)canBecomeFirstResponder;
-- (NSMethodSignature*)methodSignatureForSelector:(SEL)selector;
+-(NSMethodSignature*)methodSignatureForSelector:(SEL)selector;
 - (void)forwardInvocation:(NSInvocation *)anInvocation;
 - (void)stopCapture;
 - (void)startCapture;
@@ -71,79 +80,100 @@ int fd;
 @implementation ShellView : UITextView 
 - (id)initWithFrame:(struct CGRect)fp8
 {
-  NSLog(@"Created ShellView");
-  _nextCommand = [[NSMutableString stringWithCapacity:255] retain];
-  _ignoreInsertText = NO;
-  return [super initWithFrame:fp8];
+	debug(@"Created ShellView");
+	_nextCommand = [[NSMutableString stringWithCapacity:255] retain];
+	_ignoreInsertText = NO;
+	_controlKeyMode = NO;
+	return [super initWithFrame:fp8];
 }
 
 - (BOOL)canBecomeFirstResponder 
 {
-  return NO;
+	return NO;
 }
 
 - (void)stopCapture
 {
-  _ignoreInsertText = YES;
+	_ignoreInsertText = YES;
 }
 
 - (void)startCapture
 {
-  _ignoreInsertText = NO;
+	_ignoreInsertText = NO;
 }
 
 -(NSMethodSignature*)methodSignatureForSelector:(SEL)selector
 {
-  NSLog(@"Requested method for selector: %@", NSStringFromSelector(selector));
-  return [super methodSignatureForSelector:selector];
+	debug(@"Requested method for selector: %@", NSStringFromSelector(selector));
+	return [super methodSignatureForSelector:selector];
 }
 
 - (BOOL)respondsToSelector:(SEL)aSelector
 {
-  //NSLog(@"Request for selector: %@", NSStringFromSelector(aSelector));
-  return [super respondsToSelector:aSelector];
+	//debug(@"Request for selector: %@", NSStringFromSelector(aSelector));
+	return [super respondsToSelector:aSelector];
 }
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation
 {
-  NSLog(@"Called from UITextView %@",
-        NSStringFromSelector([anInvocation selector]));
-  [super forwardInvocation:anInvocation];
+	debug(@"Called from UITextView %@", NSStringFromSelector([anInvocation selector]));
+	[super forwardInvocation:anInvocation];
+	return;
 }
 
 - (BOOL)webView:(id)fp8 shouldDeleteDOMRange:(id)fp12
 {
-  //NSLog(@"deleting range: %i, %i", [fp12 startOffset], [fp12 endOffset]);
+	debug(@"deleting range: %i, %i", [fp12 startOffset], [fp12 endOffset]);
 
-  if (!_ignoreInsertText) {
-    const char delete_cstr = 0x08;
-    if (write(fd, &delete_cstr, 1) == -1) {
-      perror("write");
-      exit(1);
-    }
-  }
-  return [super webView:fp8 shouldDeleteDOMRange:fp12];
+	if(!_ignoreInsertText) {
+		const char delete_cstr = 0x08;
+	    if (write(fd, &delete_cstr, 1) == -1) {
+	      perror("write");
+	      exit(1);
+	    }
+	}
+	return [super webView:fp8 shouldDeleteDOMRange:fp12];
 }
 
-- (BOOL)webView:(id)fp8 shouldInsertText:(id)character
-                       replacingDOMRange:(id)fp16
-                             givenAction:(int)fp20
+- (BOOL)webView:(id)fp8 shouldInsertText:(id)character replacingDOMRange:(id)fp16 givenAction:(int)fp20
 {
-  //NSLog(@"range while inserting: %p, %x, %x", fp16, fp16->location, fp16->length);
-  //NSLog(@"range class? %@", [fp16 class]);
-  //NSLog(@"range: %i, %i", [fp16 startOffset], [fp16 endOffset]);
-  //NSLog(@"inserting.. %@", character);
-  if (!_ignoreInsertText) {
-    const char* cmd_cstr =
-      [character cStringUsingEncoding:[NSString defaultCStringEncoding]];
-    if (write(fd, cmd_cstr, [character length]) == -1) {
-      perror("write");
-      exit(1);
-    }
-    return NO;
-  }
-  return [super webView:fp8 shouldInsertText:character
-             replacingDOMRange:fp16 givenAction:fp20];
+	//debug(@"range while inserting: %p, %x, %x", fp16, fp16->location, fp16->length);
+	//debug(@"range class? %@", [fp16 class]);
+	//debug(@"range: %i, %i", [fp16 startOffset], [fp16 endOffset]);
+	debug(@"inserting.. %#x", [character characterAtIndex:0]);
+	
+	if(!_ignoreInsertText) {
+		if([character length] > 1) return false;	//or just loop through
+	
+		char cmd_char = [character characterAtIndex:0];
+	
+		if(!_controlKeyMode) {
+			if([character characterAtIndex:0] == 0x2022) {
+				//debug(@"ctrl key mode");
+				_controlKeyMode = YES;
+				return NO;
+			}
+		} else {
+			//was in ctrl key mode, got another key
+			//debug(@"sending ctrl key");
+			if(cmd_char < 0x60 && cmd_char > 0x40) {
+				//Uppercase
+				cmd_char -= 0x40;
+			} else if(cmd_char < 0x7B && cmd_char > 0x61) {
+				//Lowercase
+				cmd_char -= 0x60;
+			}
+			_controlKeyMode = NO;
+		}
+		
+		debug(@"writing char: %#x", cmd_char);
+	    if (write(fd, &cmd_char, 1) == -1) {
+	      perror("write");
+	      exit(1);
+	    }
+		return NO;		
+	}
+	return [super webView:fp8 shouldInsertText:character replacingDOMRange:fp16 givenAction:fp20];
 }
 @end
 
@@ -160,7 +190,7 @@ ShellView* view;
 // input from the user.  When it detects the user has pressed return, it
 // sends the command to the background shell.
 - (void)heartbeatCallback:(id)ignored
-{
+{	
   char buf[255];
   int nread;
 
@@ -174,28 +204,46 @@ ShellView* view;
        exit(1);
     }
     buf[nread] = '\0';
-    NSString* out = [[NSString stringWithCString:buf
-                        encoding:[NSString defaultCStringEncoding]] retain];
-    if ([out length] == 3) {
-      if ([out characterAtIndex:0] == 0x08 &&
-          [out characterAtIndex:1] == 0x20 &&
-          [out characterAtIndex:2] == 0x08) {
-        // delete sequence, don't output
-        //NSLog(@"delete");
-        continue;
-      }
-    }
+    NSString* out = [[NSString stringWithCString:buf encoding:[NSString defaultCStringEncoding]] retain];
+    //debug(out);
+    //NSString* text = [[[NSString alloc] initWithString:[view text]] retain];
+    //text = [[text stringByAppendingString: out] retain];
+	//[view setEditable:YES];
+	
+	if([out length] == 1) {
+		debug(@"length 1, char code %u", [out characterAtIndex:0]);		
+	} else {
+		debug(@"length of %d", [out length]);
+		int i;
+		for(i = 0; i < [out length]; i++) {
+			debug(@"char %d: code %u", i, [out characterAtIndex:i]);	
+		}
+	}
+	
+	//seems like if i read out a empty buffer with errno = EAGAIN it means exit
+	if(![out length]) {
+		//doesn't zoom out, is there a UIApplication method?
+		exit(1);
+	}
+	
+	if([out length] == 3) {
+		if([out characterAtIndex:0] == 0x08 && [out characterAtIndex:1] == 0x20 && [out characterAtIndex:2] == 0x08) {
+			//delete sequence, don't output
+			//debug(@"delete");
+			continue;
+		}
+	}
+	
+	[[[view _webView] webView] moveToEndOfDocument:self];
+	[view stopCapture];
+	[[view _webView] insertText: out];
+	[view startCapture];
 
-    [[[view _webView] webView] moveToEndOfDocument:self];
-    [view stopCapture];
-    [[view _webView] insertText:(id)out];
-    [view startCapture];
-
-    NSRange aRange;
-    aRange.location = 9999999;	//horray for magic number
-    aRange.length = 1;
-    [view setSelectionRange:aRange];
-    [view scrollToMakeCaretVisible:YES];
+	NSRange aRange;
+	aRange.location = 9999999;	//horray for magic number
+	aRange.length = 1;
+	[view setSelectionRange:aRange];
+	[view scrollToMakeCaretVisible:YES];
   }
 }
 
@@ -210,19 +258,18 @@ ShellView* view;
     [window orderFront: self];
     [window makeKey: self];
     [window _setHidden: NO];
-
-    float black[4] = {0, 0, 0, 1};
-    float white[4] = {1, 1, 1, 1};
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+	//make colors	
+	float backcomponents[4] = {0, 0, 0, 1};
+	float textcomponents[4] = {1, 1, 1, 1};
+	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
 	
 
-    view = [[ShellView alloc]
-               initWithFrame: CGRectMake(0.0f, 0.0f, 320.0f, 240.0f)];
-    [view setText:@"Welcome!\n"];
+    view = [[ShellView alloc] initWithFrame: CGRectMake(0.0f, 0.0f, 320.0f, 240.0f)];
+    [view setText:@""];
     [view setTextSize:12];
-	[view setTextColor:  CGColorCreate( colorSpace, white)];
+	[view setTextColor:  CGColorCreate( colorSpace, textcomponents)];
     [view setTextFont:@"Courier"];
-	[view setBackgroundColor: CGColorCreate( colorSpace, black)];
+	[view setBackgroundColor: CGColorCreate( colorSpace, backcomponents)];
     [view setEditable:YES];  // don't mess up my pretty output
 	[view setAllowsRubberBanding:YES];
     [view displayScrollerIndicators];
@@ -256,9 +303,9 @@ ShellView* view;
       exit(1);
     }
 
-    //DelegateDebug* debugDelegate = [[DelegateDebug alloc] retain];
-    //[debugDelegate doSomethingWeird];
-
+	//DelegateDebug* debugDelegate = [[DelegateDebug alloc] retain];
+	//[debugDelegate doSomethingWeird];
+	
     ShellKeyboard* keyboard = [[ShellKeyboard alloc]
         initWithFrame: CGRectMake(0.0f, 240.0, 320.0f, 480.0f)];
 
