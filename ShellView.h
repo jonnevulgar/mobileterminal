7c496380c7b4dbbe480bc9f386c8f2718d159d47
craigcbrunner <craigcbrunner@58fa8ae0-6a36-0410-a7a1-b30e0234c60e>
2007-08-02 07:31:05 +0000

    Forgot to add this sorry.

---
+++ ShellView.h
@@ -0,0 +1,31 @@
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UIKit/UIApplication.h>
+#import <UIKit/UIPushButton.h>
+#import <UIKit/UITableCell.h>
+#import <UIKit/UIImageAndTextTableCell.h>
+#import <UIKit/UIView.h>
+#import <UIKit/UIView-Hierarchy.h>
+#import <UIKit/UITextView.h>
+
+@interface ShellView : UITextView {
+	NSMutableString* _nextCommand;
+	bool _ignoreInsertText;
+	bool _controlKeyMode;
+}
+
+-(id)initWithFrame:(struct CGRect)fp8;
+- (BOOL)canBecomeFirstResponder;
+-(NSMethodSignature*)methodSignatureForSelector:(SEL)selector;
+- (void)forwardInvocation:(NSInvocation *)anInvocation;
+- (void)stopCapture;
+- (void)startCapture;
+
+- (BOOL)respondsToSelector:(SEL)aSelector;
+- (void)forwardInvocation:(NSInvocation *)anInvocation;
+- (BOOL)webView:(id)fp8 shouldDeleteDOMRange:(id)fp12;
+
+  -(BOOL)webView:(id)fp8 shouldInsertText:(id)character replacingDOMRange:(id)fp16 givenAction:(int)fp20;
+
+@end
