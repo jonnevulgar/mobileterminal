cbafac6cb326bf3f777eed0a0c1db14385a93c66
allen.porter <allen.porter@58fa8ae0-6a36-0410-a7a1-b30e0234c60e>
2007-08-02 07:35:35 +0000

    Removed un-needed stuff, and style cleanups

--- ShellView.h
+++ ShellView.h
@@ -1,31 +1,23 @@
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <UIKit/UIApplication.h>
-#import <UIKit/UIPushButton.h>
-#import <UIKit/UITableCell.h>
-#import <UIKit/UIImageAndTextTableCell.h>
-#import <UIKit/UIView.h>
-#import <UIKit/UIView-Hierarchy.h>
 #import <UIKit/UITextView.h>
 
 @interface ShellView : UITextView {
-	NSMutableString* _nextCommand;
-	bool _ignoreInsertText;
-	bool _controlKeyMode;
+  NSMutableString* _nextCommand;
+  bool _ignoreInsertText;
+  bool _controlKeyMode;
 }
 
--(id)initWithFrame:(struct CGRect)fp8;
+- (id)initWithFrame:(struct CGRect)fp8;
 - (BOOL)canBecomeFirstResponder;
--(NSMethodSignature*)methodSignatureForSelector:(SEL)selector;
+- (NSMethodSignature*)methodSignatureForSelector:(SEL)selector;
 - (void)forwardInvocation:(NSInvocation *)anInvocation;
 - (void)stopCapture;
 - (void)startCapture;
-
 - (BOOL)respondsToSelector:(SEL)aSelector;
 - (void)forwardInvocation:(NSInvocation *)anInvocation;
 - (BOOL)webView:(id)fp8 shouldDeleteDOMRange:(id)fp12;
-
-  -(BOOL)webView:(id)fp8 shouldInsertText:(id)character replacingDOMRange:(id)fp16 givenAction:(int)fp20;
+- (BOOL)webView:(id)fp8 shouldInsertText:(id)character
+                       replacingDOMRange:(id)fp16 givenAction:(int)fp20;
 
 @end
