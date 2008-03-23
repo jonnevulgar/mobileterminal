90d880e44734687d016063f1a0add447e0e0df12
monsterkodi <monsterkodi@58fa8ae0-6a36-0410-a7a1-b30e0234c60e>
2008-03-23 02:50:59 +0000

    preferences page prepared

---
+++ Constants.h
@@ -0,0 +1,8 @@
+//
+//  Constants.h
+//  Terminal
+
+#define TOGGLE_KEYBOARD_DELAY  0.35
+#define PIE_MENU_DELAY         0.45 
+#define PIE_MENU_FADE_IN_TIME  0.25
+#define PIE_MENU_FADE_OUT_TIME 0.25
